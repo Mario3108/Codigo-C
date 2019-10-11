515a9f40ad05554ed4c06fff046d551f4e6970be
Mario3108 <mrmike3108@gmail.com>
2019-10-11 14:09:47 +0200

    ASFD

--- findCommand.c
+++ findCommand.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+
 
 int main(int argc, char const *argv[]) {
-  
+
+  if (argc != 2) {
+    printf("Error: intruce un comando .\n", );
+  }else{
+    char comando[] = argv[1];
+  }
+
+  char dir[] = getenv("PATH");
+
+
   return 0;
 }
