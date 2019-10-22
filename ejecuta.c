78d1e521af21738989b0cd34964339a66b996be6
Mario3108 <mrmike3108@gmail.com>
2019-10-22 11:53:28 +0200

    ejecuta

---
+++ ejecuta.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[]) {
+  pid_t pid;
+  int status;
+
+  if (argc < 2) {
+    fprintf(stderr, "Uso: %s comando y opciones\n", argv[0]);
+    return 1;
+  }
+
+  pid=fork();
+
+  if(pid < 0){
+      fprintf(stderr, "Fallo en el fork)().\n%s\n", stderr(errno));
+      exit(1);
+  }else if (pid == 0){
+    execvp(argv[1], argv + 1);
+
+    printf("Error al ejecutar el comando: %s\n", stderror(errno));
+    exit(1);
+  }else{
+    wait(&status);
+
+    if (WIFESTATUS(status) != 0) {
+      if(WIFEXITED(status) != 0){
+        printf("El comando  no se ejecuto correctamente\n" );
+      }
+    }
+    exit(0);
+
+  }
+
+  return 0;
+}
