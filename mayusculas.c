4bc1e231c36bcb0f9d4f2fd850f2b4caa243afa3
Mario3108 <mrmike3108@gmail.com>
2019-10-25 13:45:45 +0200

    sopotamadre

--- mayusculas.c
+++ mayusculas.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
 
 int main(int argc, char const *argv[]) {
   int status, pipe_des1[2], pipe_des2[2];
@@ -10,10 +11,11 @@ int main(int argc, char const *argv[]) {
   FILE *p_p, *p_h;
   char buf[1024];
   if (argc > 1) {
-    fprintf(stderr, "No son necesarios argumentos para ejecutar este comando\n", );
+    fprintf(stderr, "No son necesarios argumentos para ejecutar este comando\n", strerror(errno));
     return 1;
   }
-  pipe(pipe_des);
+  pipe(pipe_des1);
+  pipe(pipe_des2);
   pid = fork();
   if(pid < 0){
     fprintf(stderr, " Ha habido un error en el fork \n %s\n", strerror(errno));
@@ -23,10 +25,19 @@ int main(int argc, char const *argv[]) {
     close(pipe_des2[0]);
     p_p = fdopen(pipe_des1[0], "r");
     p_h = fdopen(pipe_des2[1], "w");
+    char buf2[1024];
 
-    while( fgets(buf, 1024, p_p) != NILL){
-
+    while( fgets(buf, 1024, p_p) != NULL){
+        for(int i = 0; i<1024;i++){
+          buf2[i] = toupper(buf[i]);
+        }
+        fputs(buf2, p_h);
+        fflush(p_h);
     }
+
+
+
+
 
   }else{
     close(pipe_des1[0]);
