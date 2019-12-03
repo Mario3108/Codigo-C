e59beda15e6641d30d4a73f55bf6ec4395f43e49
Mario3108 <mrmike3108@gmail.com>
2019-12-03 11:31:07 +0100

    puesta a punto

--- EjerciciosThreads/parking.c
+++ EjerciciosThreads/parking.c
@@ -7,15 +7,10 @@
 int estado_coches[COCHES];
 int aparcamiento[COCHES/2];
 int plazas_libres = COCHES/2;
+
 pthread_mutex_t mutex[COCHES];
 pthread_cond_t espera[COCHES];
 
-//char *estado(int i){
-  //  if(aparcamiento[i] != 0){
-
-  //  }
-//}
-//Funcion para imprimir la lista de aparcamientos y ver cuales están ocupados
 void print_estado(){
     printf("Parking: ");
     for(int i= 0; i<COCHES; i++){
@@ -24,27 +19,37 @@ void print_estado(){
     printf("\n");
 }
 
-void *coche(void *num){
-    int id = *(int *) num;
+void *funcion(void *num){
+    int coche_id = *(int *) num;
+    int ocupado = 1;
+    while(1){
+        estado_coches[coche_id] = 0;
+        print_estado();
+        
+        while(ocupado){
+            for(int i = 0; i<COCHES/2; i++){
+                if(aparcamiento[i] == 0){
+                    pthread_mutex_lock(&mutex[i]);
+                }
+            }
+        }
+    }
 }
 
-
 int main(){
-    int i;
     pthread_t th;
-    int id_coches[COCHES];
-
+    int i;
+    int coche_id[COCHES];
+    pthread_mutex_init(&mutex, NULL);
     for(i = 0; i<COCHES; i++){
-        
-        pthread_mutex_init(&mutex[i], NULL);
-        aparcamiento[i] = 0;
-        pthread_cond_init(&espera, NULL);
-        id_coches[i] = i;
-        pthread_create(&th, NULL, coche, (void *) &id_coches[i]);
-        
-    
+        aparcamient[i] = 0;
+        pthread_cond_init(&espera,  NULL);
+        coche_id[i] = i;
+        pthread_create(&th, NULL, funcion, (void *) &coche_id[i]);
     }
 
     while(1);
+    //comentario
 
 }
+
